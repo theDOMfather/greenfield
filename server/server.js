2eeb654968b012883750fadb48a74eb64bc1bd62
Collin Tharp <catharp@gmail.com>
2016-12-01 17:30:17 -0800

    Implement basic facebook login

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ var path = require ('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+require('./auth/auth.js')(passport);
+
 var app = express();
 
-var port = process.env.PORT || 8000; 
+var port = process.env.PORT || 8080;
 
 
 app.use('/', express.static(path.join(__dirname, '../client')));
@@ -14,6 +17,7 @@ app.use('/modules', express.static(path.join(__dirname, '../node_modules')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
 
 
 app.get('/', function(req, res) {
@@ -21,9 +25,14 @@ app.get('/', function(req, res) {
 	res.end();
 });
 
+// authentication routes
+app.get('/auth/facebook', passport.authenticate('facebook', {
+  scope : 'email' }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect : '/',
+  failureRedirect : '/fail',   // ********* REPLACE with working route once created ***********
+  session: false
+}));
 
-
-app.listen(port); 
-console.log('Listing to ....' + port ); 
-
-
+app.listen(port);
+console.log('Listing to ....' + port );
