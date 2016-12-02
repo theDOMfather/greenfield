7f6e28b70d1c46fbb5b84eb7e8594fc4413156fa
Collin Tharp <catharp@gmail.com>
2016-12-01 19:33:51 -0800

    Implement sessions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var http = require ('http');
 var path = require ('path');
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var passport = require('passport');
@@ -17,8 +18,9 @@ app.use('/modules', express.static(path.join(__dirname, '../node_modules')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({ secret: 'squirrel' }));
 app.use(passport.initialize());
-
+app.use(passport.session());
 
 app.get('/', function(req, res) {
 	res.write('hello');
@@ -30,8 +32,7 @@ app.get('/auth/facebook', passport.authenticate('facebook', {
   scope : 'email' }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect : '/',
-  failureRedirect : '/fail',   // ********* REPLACE with working route once created ***********
-  session: false
+  // failureRedirect : '/fail'
 }));
 
 app.listen(port);
