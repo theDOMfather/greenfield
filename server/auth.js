e787ef0e0eb5c13f8755ffc0cac3a44f8f6a1b36
Collin Tharp <catharp@gmail.com>
2016-12-05 12:26:42 -0800

    Move database schema into modular file, refactor server code for clarity and add new users to db during auth

--- server/auth.js
+++ server/auth.js
@@ -1,11 +1,11 @@
-var FacebookStrategy = require('passport-facebook').Strategy;
-var LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
 
 // load user model
-//var User = require('../../client/userModel');
+const User = require('./userModel.js');
 
 // load API keys
-var Keys = require('./keys');
+const Keys = require('./keys');
 
 module.exports = function(passport) {
 
@@ -16,7 +16,7 @@ module.exports = function(passport) {
 
    // used to deserialize the user
    passport.deserializeUser(function(id, done) {
-       User.findById(id, function(err, user) {
+       User.find({ 'id': id }, function(err, user) {
            done(err, user);
        });
    });
@@ -32,27 +32,25 @@ module.exports = function(passport) {
   function(token, refreshToken, profile, done) {
     process.nextTick(function() {
       // find the user in our database
-      // User.findOne({ 'id' : profile.id }, function(err, user) {
-      //   if (err) {                   // if error connecting to database, throw error
-      //     return done(err);
-      //   } else if (user) {           // if the user is found, log them in
-      //     return done(null, user);
-      //   } else {                     // if no user is found with that facebook id, create them as a new user
+      User.findOne({ 'id' : profile.id }, function(err, user) {
+        if (err) {                   // if error connecting to database, throw error
+          return done(err);
+        } else if (user) {           // if the user is found, log them in
+          return done(null, user);
+        } else {                     // if no user is found with that facebook id, create them as a new user
           var newUser = {};
           newUser.token = token;
           newUser.id = profile.id;
-          newUser.name = profile.displayName; // look at the passport user profile to see how names are returned
+          newUser.name = profile.displayName;
           //newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
-          return done(null, newUser);
-
           // save user to the database
-      //     newUser.save(function(err) {
-      //       if (err) throw err;
-      //       else return done(null, newUser);
-      //     });
-      //   }
-      // });
+          new User(newUser).save(function(err) {
+            if (err) throw err;
+            else return done(null, newUser);
+          });
+        }
+      });
     });
   }));
 };
