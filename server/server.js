9c0d07983b44aef88103e611221d970fa70b4bcf
bartekringwelski <bartekr@gmail.com>
2016-12-06 20:02:30 -0800

    ...

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ app.get('/messageToConsole', function(req, res) {
         phoneNumber: shortPhone
       }, {
         $set: {
-          "responses.3": "1"
+          responses: [1, 2, 4]
         }
       });
 
