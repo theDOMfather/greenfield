1bc15e573958f954c306b4ba6a5ead1df7ff8a42
bartekringwelski <bartekr@gmail.com>
2016-12-07 12:12:13 -0800

    restuyled homepage

--- server/sms/sms.js
+++ server/sms/sms.js
@@ -16,10 +16,6 @@ exports.sendWelcome = function(userPhoneNumber) {
   }, function(err, responseData) { //this function is executed when a response is received from Twilio
 
     if (!err) { // "err" is an error received during the request, if any
-
-      // "responseData" is a JavaScript object containing data received from Twilio.
-      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
       console.log(responseData.from); // outputs "+14506667788"
       console.log(responseData.body); // outputs "word to your mother."
 
@@ -41,11 +37,6 @@ exports.periodicGoalPoll = function(userPhoneNumber, userGoal) {
   }, function(err, responseData) { //this function is executed when a response is received from Twilio
 
     if (!err) { // "err" is an error received during the request, if any
-
-      // "responseData" is a JavaScript object containing data received from Twilio.
-      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-      // console.log(responseData.from); // outputs "+14506667788"
       console.log("reponse from user's phone number:");
       console.log(responseData.body); // outputs "word to your mother."
 
