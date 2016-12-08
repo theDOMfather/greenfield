ea43526578bc00f9f68a3fc63e2f789f30d03851
Abiy Melaku <agirma08@gmail.com>
2016-12-08 14:36:13 -0800

    small changes made

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -6,7 +6,7 @@ angular.module("app.status", [])
   $http.get('/user')
     .success(function(user) {
       $scope.user = user;
-      console.log(user.responses);
+      console.log('user.responses:  ',user.responses);
     })
     .error(function(data) {
       console.error(err);
