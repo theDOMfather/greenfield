412aa900d4bc309796f53070acc7245f071c5f1a
Collin Tharp <catharp@gmail.com>
2016-12-09 16:50:48 -0800

    Clean up client-side console.logs

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -3,7 +3,7 @@ angular.module("app.status", [])
 .controller("statusController", function($scope, $http) {
   $scope.user = {};
   $http.get('/user')
-    .success((user) => {$scope.user = user; console.log("this is from status.js", $scope.user)})
+    .success((user) => $scope.user = user)
     .error((err) => console.error(err));
 
 });
