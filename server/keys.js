9f5d1ed329d44560f7a3a50a884bd96ebe23b68e
bartekringwelski <bartekr@gmail.com>
2016-12-09 20:39:54 -0800

    keys

--- server/keys.js
+++ server/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebook': {
         'clientID': '1020549974735073',
         'clientSecret': 'e57b9fddc935f1564464fddb7b33e788',
-        'callbackURL': 'http://localhost:8000/auth/facebook/callback'
+        'callbackURL': 'https://peaceful-caverns-27176.herokuapp.com/auth/facebook/callback'
     },
     'twilio': {
         'TWILIO_ACCOUNT_SID': 'AC388c3657dba095065282b969d5985a90',
