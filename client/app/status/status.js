a5ffb86f9ad079b78f3473a7b08204148fe4ef3d
Collin Tharp <catharp@gmail.com>
2016-12-09 20:18:31 -0800

    Style progress bar to change color according to user grade

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -6,6 +6,13 @@ angular.module("app.status", [])
     .success((user) => {
       $scope.user = user;
       $scope.user.style = 'width:' + $scope.user.grade + '%;';
+      if (user.grade > 70) {
+        $scope.user.class = 'progress-bar progress-bar-success active';
+      } else if (user.grade > 40) {
+        $scope.user.class = 'progress-bar progress-bar-warning active';
+      } else {
+        $scope.user.class = 'progress-bar progress-bar-danger active';
+      }
     })
     .error((err) => console.error(err));
 
