00a65aeaa9e5bd28ed2e1144e4bde85978e4246e
Collin Tharp <catharp@gmail.com>
2016-12-09 19:45:08 -0800

    Implement working progress bar

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -3,12 +3,17 @@ angular.module("app.status", [])
 .controller("statusController", function($scope, $http) {
   $scope.user = {};
   $http.get('/user')
-
-    .success(function(user) {
-    	$scope.user = user;
-    	$scope.user.responses = $scope.user.responses.map(function(item){
-    		return [moment(item[0]).format(), item[1]];
-    	});
-    });
-
+    .success((user) => {
+      $scope.user = user;
+      $scope.user.style = 'width:' + $scope.user.grade + '%;';
+      if (user.grade > 70) {
+        $scope.user.class = 'progress-bar progress-bar-success active';
+      } else if (user.grade > 40) {
+        $scope.user.class = 'progress-bar progress-bar-warning active';
+      } else {
+        $scope.user.class = 'progress-bar progress-bar-danger active';
+      }
+    })
+    .error((err) => console.error(err));
+    
 });
