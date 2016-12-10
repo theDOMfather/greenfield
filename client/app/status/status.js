4fd7ecc08ef3e824a6151633cc186fe3862753c7
Collin Tharp <catharp@gmail.com>
2016-12-10 13:35:43 -0800

    Fix responses on status page

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -5,15 +5,16 @@ angular.module("app.status", [])
   $http.get('/user')
     .success((user) => {
       $scope.user = user;
-      $scope.user.style = 'width:' + $scope.user.grade + '%;';
+      $scope.responses = user.responses;
+      $scope.progBarStyle = 'width:' + $scope.user.grade + '%;';
       if (user.grade > 70) {
-        $scope.user.class = 'progress-bar progress-bar-success active';
+        $scope.progBarClass = 'progress-bar progress-bar-success active';
       } else if (user.grade > 40) {
-        $scope.user.class = 'progress-bar progress-bar-warning active';
+        $scope.progBarClass = 'progress-bar progress-bar-warning active';
       } else {
-        $scope.user.class = 'progress-bar progress-bar-danger active';
+        $scope.progBarClass = 'progress-bar progress-bar-danger active';
       }
     })
     .error((err) => console.error(err));
-    
+
 });
