daa4d3e418c28f770c95caf4723edc74e4503c0a
Collin Tharp <catharp@gmail.com>
2016-12-09 19:12:02 -0800

    Add link to complete goal button, format phone numbers on status page, and add login redirect for '/' paths

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,9 @@ angular.module('app', [
 .config(function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl:"login/index.html"
+		controller: function() { window.location.replace('/'); },
+    template: '<div></div>'
+
 	})
 	.when('/create', {
 		templateUrl:"app/create/create.html",
