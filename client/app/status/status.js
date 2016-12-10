64a1ae4e63d15da2a7856b2f298df7d1c73faf7a
Collin Tharp <catharp@gmail.com>
2016-12-09 19:45:08 -0800

    Implement working progress bar

--- client/app/status/status.js
+++ client/app/status/status.js
@@ -3,7 +3,10 @@ angular.module("app.status", [])
 .controller("statusController", function($scope, $http) {
   $scope.user = {};
   $http.get('/user')
-    .success((user) => $scope.user = user)
+    .success((user) => {
+      $scope.user = user;
+      $scope.user.style = 'width:' + $scope.user.grade + '%;';
+    })
     .error((err) => console.error(err));
 
 });
