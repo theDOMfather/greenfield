d093efaadcaf089976cc993c6878a1daf753e6a5
Collin Tharp <catharp@gmail.com>
2016-12-09 16:19:25 -0800

    Delete zombie files, rename login files for clarity, and round corners of FB button

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,7 @@ angular.module('app', [
 .config(function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl:"app/login.html"
+		templateUrl:"login/index.html"
 	})
 	.when('/create', {
 		templateUrl:"app/create/create.html",
